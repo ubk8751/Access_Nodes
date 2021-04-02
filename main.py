bceaf84030e89ce02d2e47d9c3a22396394e550e
ubk8751 <ubk8751@gmail.com>
2021-04-02 19:15:24 +0200

    get_all_names created

--- main.py
+++ main.py
@@ -1,15 +1,32 @@
+'''
+By:     Samuel Jonsson
+        Blekinge Tekniska Högskola
+Date:   2021-04-02
+Email:  ubk8751@gmail.com
+'''
+
 # Import the good stuff
 import requests
 import json
 
 def get_data(unit_name):
+    '''
+    Description:
+        A function for gettin information about a sensor, or all sensors connected 
+        to the account
+
+    Args:
+        unit_name (string): the name of the sensor that we want information about. Leave blank for complete list.
+
+    Return:
+        Prints the name of the sensor(s). 
+    '''
+
     # Get the authorisation file
     try:
         f = open("token.json")
         dct = json.load(f)
         f.close()
-    
-        # Return the Json file
     
     # If there is an error in loading the authorisation file
     except:
@@ -50,7 +67,61 @@ def get_data(unit_name):
         for i in range(len(data)):
             print(data[i]["name"])
 
+def get_all_names():
+    '''
+    Description:
+        A function for printing all the sensors connected to the user.
+
+    Return:
+        Prints the name of all the sensors connected to the account. 
+    '''
+
+    # Get the authorisation file
+    try:
+        f = open("token.json")
+        dct = json.load(f)
+        f.close()
+    
+    # If there is an error in loading the authorisation file
+    except:
+        print("There was an error in loading the token file!")
+    
+    # Necessary link, don't touch
+    url = "https://api.yggio-sandbox.sensative.net/api/iotnodes"
+    
+    # Create the token
+    token = "Bearer " + dct["token"]
+
+    # Create header info
+    header = {
+            "Authorization": token
+            }
+    
+    # Get the request object containing the info needed
+    request = requests.get(url, headers=header)
+    
+    # Check if the request status is ok
+    if request.status_code == 200:
+        data = request.json()
+
+        # In this case, print the name of all sensors, 
+        # but this can be changed depending on what you want
+        for i in range(len(data)):
+            print(data[i]["name"])
+
 def create_auth(u_name, p_word):
+    '''
+    Description:
+        A function for creating the authentication information for the API.
+
+    Args:
+        u_name (string): username of the account
+        p_word (string): password of the account
+
+    Return:
+        Creates a JSON file with the authentication information. 
+    '''
+
     # Needed stuff, don't touch
     header = {
         "accept": "application/json",
@@ -93,14 +164,19 @@ def create_auth(u_name, p_word):
             print("I/O error")
 
 if __name__ == "__main__":
-    # u_name is the name of the sensor, leave blank to print complete 
-    # list of sensors connected with the account. Only include one sensor at a time.
-    # u_name = ""
+    '''unit_name is the name of the sensor, leave blank to print complete 
+    list of sensors connected with the account. Only include one sensor at a time.'''
     
-    u_name = "test@test.com"
-    p_word = "test"
+    # unit_name = ""
+    # get_data(unit_name)
+    
+    '''u_name is the username and p_word is the password that will 
+    be used for logging in'''
+    
+    # u_name = "test@test.com"
+    # p_word = "test"
+    # create_auth(u_name, p_word)
 
-    # Choose what you wnat to do
-    create_auth(u_name, p_word)
-    # get_data(u_name)
+    '''Easy way to print all of the sensors connected to the user'''
+    get_all_names()
 
